4b09fd922ddae410160b7d7547a72af58231fd2b
DUSHIME Don Aime Hosanna <118371312+DUSHIME1212@users.noreply.github.com>
2024-10-12 16:05:54 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,35 @@
 #!/usr/bin/env bash
-# Script using bash to setup webservers for deployment of simple HTML
-# content
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-apt-get update && \
-apt-get install -y nginx && \
-mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
-echo 'Testing 123' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
-sed -i "37i $insert" /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Add test HTML file to the test folder
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+
+# Create a symbolic link to /data/web_static/current
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+
+# Test the Nginx configuration to ensure there are no syntax errors
+sudo nginx -t
+
+# Restart Nginx to apply the changes
+sudo service nginx restart
